e0c8390c99ef34e83e9a1c7f644139758d18b32b
Luis Urán <luisuran.bit@gmail.com>
2023-02-09 10:39:51 -0300

    added factories for Company and Employee

---
+++ database/factories/EmployeeFactory.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Company;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Employee>
+ */
+class EmployeeFactory extends Factory
+{
+    public function definition()
+    {
+        return [
+            'first_name' => $this->faker->firstName,
+            'last_name' => $this->faker->lastName,
+            'company_id' => Company::all()->random()->id,
+            'email' => $this->faker->unique()->safeEmail,
+            'phone' => $this->faker->phoneNumber,
+        ];
+    }
+}

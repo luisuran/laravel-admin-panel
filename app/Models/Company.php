6eb50a5df8358e33e1209ee927c43a8bc1bf9a2c
Luis Urán <luisuran.bit@gmail.com>
2023-02-12 13:10:17 -0300

    added relations between Company and Employee

--- app/Models/Company.php
+++ app/Models/Company.php
@@ -10,4 +10,9 @@ class Company extends Model
     use HasFactory;
 
     protected $fillable = [ 'name', 'email', 'logo', 'website' ];
+
+    public function employees()
+    {
+        return $this->hasMany(Employee::class);
+    }
 }
--- app/Models/Employee.php
+++ app/Models/Employee.php
@@ -10,4 +10,9 @@ class Employee extends Model
     use HasFactory;
 
     protected $fillable = [ 'first_name', 'last_name', 'company_id', 'email', 'phone' ];
+
+    public function company()
+    {
+        return $this->belongsTo(Company::class);
+    }
 }
